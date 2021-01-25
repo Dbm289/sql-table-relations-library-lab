2fd1e063cbfae42fd524c3f450f550d7498cc544
Dan <DanielMooreRealPerson@gmail.com>
2021-01-25 11:35:44 -0500

    commit tree 4c12be5f5f3c34c5e8f66f35e62395ebdd9f32f6

--- lib/querying.rb
+++ lib/querying.rb
@@ -24,14 +24,36 @@ def select_name_and_series_subgenres_of_authors
 end
 
 def select_series_title_with_most_human_characters
-  "SELECT series.title FROM series JOIN books ON books.series_id = series.id 
-  JOIN character_books ON character_books.books_id = character_books.id
-  JOIN characters ON characters_books.character_id = characters.id 
-  ORDER BY COUNT(WHERE characters.species = 'human')
-  LIMIT 1
-  ;"
+ # "SELECT series.title FROM series JOIN books ON books.series_id = series.id 
+ # JOIN character_books ON character_books.books_id = character_books.id
+  #JOIN characters ON characters_books.character_id = characters.id 
+  #ORDER BY COUNT(WHERE characters.species = 'human')
+  #LIMIT 1
+  #;"
+
+  "SELECT series.title
+    FROM series
+    JOIN books
+    ON books.series_id = series.id
+    JOIN character_books
+    ON character_books.book_id = books.id
+    JOIN characters
+    ON character_books.character_id = characters.id
+    WHERE characters.species = 'human'
+    GROUP BY series.title
+    ORDER BY COUNT() DESC
+    LIMIT 1;"
 end
 
 def select_character_names_and_number_of_books_they_are_in
-  "Write your SQL query here"
+  "SELECT characters.name, COUNT(character_books.book_id) 
+    FROM characters
+    JOIN character_books
+    ON character_books.character_id = characters.id
+    GROUP BY characters.name
+    ORDER BY COUNT(character_books.book_id) DESC, characters.name ASC
+    ;"
 end
+
+#ORDER BY COUNT(character_books.book_id) DESC
+ #   ORDER BY character.name ASC
